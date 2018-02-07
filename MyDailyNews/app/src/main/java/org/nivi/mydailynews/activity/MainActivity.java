7777545c819e123418ac0eae0e1f193b3388c964
Nivedita-puneet <nivedita.puneet@gmail.com>
2018-02-07 23:35:31 +1100

    Created adapter to bind the data and tested.

--- MyDailyNews/app/src/main/java/org/nivi/mydailynews/activity/MainActivity.java
+++ MyDailyNews/app/src/main/java/org/nivi/mydailynews/activity/MainActivity.java
@@ -3,9 +3,14 @@ package org.nivi.mydailynews.activity;
 import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.ProgressBar;
 
 import org.nivi.mydailynews.R;
+import org.nivi.mydailynews.adapter.NewsAdapter;
+import org.nivi.mydailynews.model.Category;
 import org.nivi.mydailynews.model.News;
 import org.nivi.mydailynews.restapi.NewsAPI;
 import org.nivi.mydailynews.restapi.NewsAPIClient;
@@ -19,28 +24,46 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = MainActivity.class.getSimpleName();
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeControls();
 
         NewsAPI apiService =
                 NewsAPIClient.getClient().create(NewsAPI.class);
+
 
         Call<News> call = apiService.getUpdatedNews();
         call.enqueue(new Callback<News>() {
             @Override
             public void onResponse(Call<News> call, Response<News> response) {
                 News news = response.body();
+                List<Category> categories = news.getCategories();
+                recyclerView.setAdapter(new NewsAdapter(MainActivity.this, categories));
             }
 
             @Override
             public void onFailure(Call<News> call, Throwable t) {
-                // Log error here since request failed
                 Log.e(TAG, t.toString());
             }
         });
+
+    }
+
+    private void initializeControls() {
+
+        recyclerView = (RecyclerView) findViewById(R.id.news_recyclerview);
+        progressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
 
     }
 }
---
+++ MyDailyNews/app/src/main/java/org/nivi/mydailynews/adapter/NewsAdapter.java
@@ -0,0 +1,89 @@
+package org.nivi.mydailynews.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.nivi.mydailynews.R;
+import org.nivi.mydailynews.model.Category;
+
+import java.util.List;
+
+/**
+ * Created by NEETU on 07-02-2018.
+ */
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsAdapterViewHolder> {
+
+    private List<Category> categories;
+    private Context context;
+
+    public NewsAdapter(Context context, List<Category> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    public interface MovieAdapterOnclickHandler {
+
+        void listItemClickListener(String id);
+    }
+
+    public class NewsAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView title;
+        TextView description;
+        ImageView thumbnail;
+
+        public NewsAdapterViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.news_title);
+            description = (TextView) view.findViewById(R.id.news_desc);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            //mOnclickHandler.listItemClickListener(movies.get(getAdapterPosition()).getImdbID());
+        }
+
+    }
+
+    @Override
+    public NewsAdapter.NewsAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        int id = R.layout.news_item;
+        boolean shouldAttachToParentImmediately = false;
+        View view = LayoutInflater.from(context).inflate(id, parent, shouldAttachToParentImmediately);
+        return new NewsAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(NewsAdapter.NewsAdapterViewHolder holder, int position) {
+        holder.title.setText(categories.get(position).getTitle());
+        holder.description.setText(categories.get(position).getDescription());
+        Picasso.with(this.context).load(categories.get(position).getImageHref()).into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if (null == categories)
+            return 0;
+        else
+            return categories.size();
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+        notifyDataSetChanged();
+    }
+}
