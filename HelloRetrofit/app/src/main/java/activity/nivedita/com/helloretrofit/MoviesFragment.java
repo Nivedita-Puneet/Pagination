0fc4ae6898cdf64817de2576fd8200fcf17bf790
Nivedita-puneet <nivedita.puneet@gmail.com>
2018-09-21 00:08:16 +1000

    Added functionality to Popular Movies.

--- HelloRetrofit/app/src/main/java/activity/nivedita/com/helloretrofit/MoviesFragment.java
+++ HelloRetrofit/app/src/main/java/activity/nivedita/com/helloretrofit/MoviesFragment.java
@@ -3,11 +3,18 @@ package activity.nivedita.com.helloretrofit;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import javax.inject.Inject;
 
 import activity.nivedita.com.base.BaseFragment;
+import activity.nivedita.com.helloretrofit.presenter.MoviesBasePresenter;
 import activity.nivedita.com.model.TopRatedMovies;
 import activity.nivedita.com.networkutils.LogNetworkError;
 import dagger.android.support.AndroidSupportInjection;
@@ -18,7 +25,21 @@ import dagger.android.support.AndroidSupportInjection;
 
 public class MoviesFragment extends BaseFragment implements MovieView {
 
-    private static String TAG = MoviesFragment.class.getSimpleName();
+    @Inject
+    MoviesBasePresenter<MovieView> moviesPresenter;
+
+    @Inject
+    MovieAdapter movieAdapter;
+
+    @Inject
+    LinearLayoutManager linearLayoutManager;
+
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+
+    private int lastVisibleItem, totalItemCount;
+
+    private final int VISIBLE_THRESHOLD = 1;
 
     public static MoviesFragment newInstance() {
 
@@ -32,6 +53,8 @@ public class MoviesFragment extends BaseFragment implements MovieView {
     public void onAttach(Context context) {
         super.onAttach(context);
         AndroidSupportInjection.inject(MoviesFragment.this);
+
+
     }
 
     @Override
@@ -39,7 +62,52 @@ public class MoviesFragment extends BaseFragment implements MovieView {
                              ViewGroup container, Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.movies_fragment, container, false);
+        moviesPresenter.attachView(this);
+        View view = inflater.inflate(R.layout.activity_home, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.main_recycler);
+        progressBar = (ProgressBar) view.findViewById(R.id.main_progress);
+        setUpFragment();
+        return view;
+    }
+
+    private void setUpFragment() {
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        recyclerView.setAdapter(movieAdapter);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                totalItemCount = linearLayoutManager.getItemCount();
+                lastVisibleItem = linearLayoutManager
+                        .findLastVisibleItemPosition();
+                if (!moviesPresenter.getLoading() &&
+                        (totalItemCount <= (lastVisibleItem + VISIBLE_THRESHOLD))) {
+
+                    int pageIndex = moviesPresenter.getCurrentPage();
+                    pageIndex++;
+                    moviesPresenter.setCurrentPage(pageIndex);
+
+                    moviesPresenter.getPublishProcessor().onNext(pageIndex);
+                    //movieAdapter.addLoadingFooter();
+                    moviesPresenter.setLoading(true);
+                }
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        moviesPresenter.onViewInitialized();
     }
 
     @Override
